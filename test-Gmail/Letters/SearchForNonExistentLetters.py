30ecb06240fbcb82c694b27863c3cabb1100b939
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-12-06 16:24:48 +0300

    Add fun Auth and SearchLetters fun

--- test-Gmail/Letters/SearchForNonExistentLetters.py
+++ test-Gmail/Letters/SearchForNonExistentLetters.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
-from SearchForNonExistentLetters import auth
+
 url = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
 email = "viksharifullina@yandex.ru"
 password = "BodnarchuK2801"
@@ -14,6 +14,25 @@ chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument('disable-infobars')
 driver = webdriver.Chrome(executable_path="/test-Gmail/chromedriver.exe",
                           options=chrome_options)
+
+
+def auth(driver_data, email_data, password_data):
+    button_email = driver_data.find_element("xpath",
+                                       '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[1]/div[1]/button')
+    button_email.click()
+    email_input = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                               '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[2]/div/div[2]/span/input')))
+
+    button_next = driver_data.find_element("xpath",
+                                     '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[4]/button')
+    slow_typing(email_input, email_data)
+    button_next.click()
+    password_input = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                                  '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[2]/div[1]/span/input')))
+    slow_typing(password_input, password_data)
+    button_login = driver_data.find_element("xpath",
+                                      '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button')
+    button_login.click()
 
 
 def slow_typing(element, text):
@@ -26,8 +45,7 @@ try:
     driver.get(url=url)
     wait = WebDriverWait(driver, 20)
     auth(driver, email, password)
-
-    topic_email = "Приветствие"
+    topic_email = "sdfdfsdf"
     search_input = wait.until(ec.visibility_of_element_located((By.XPATH,
                                                                 '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/div/span/input')))
 
@@ -36,13 +54,13 @@ try:
                                        '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/button')
     buttonSearch.click()
     answer = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                          '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[1]/div')))
+                                                          '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[4]/div/div/div/h1/span')))
 
     time.sleep(5)
     if answer:
-        print("SearchLetters.py test is OK!")
+        print("SearchForNon-existendLetters.py test is OK!")
     else:
-        print("SearchLetters.py test is FAILED!")
+        print("SearchForNon-existendLetters.py test is FAILED!")
 
 except Exception as ex:
     print(ex)
