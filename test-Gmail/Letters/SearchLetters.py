1d10327af81666360b3687f91130ebd286e8e22c
ViktoriaSharifullina <viktoriasharifullina@gmail.com>
2022-12-06 16:44:44 +0300

    made some changes(don't work)

--- test-Gmail/Letters/SearchLetters.py
+++ test-Gmail/Letters/SearchLetters.py
@@ -5,15 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from SearchForNonExistentLetters import auth
-url = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
-email = "viksharifullina@yandex.ru"
-password = "BodnarchuK2801"
-chrome_options = webdriver.ChromeOptions()
-chrome_options.headless = False
-chrome_options.add_argument('--start-maximized')
-chrome_options.add_argument('disable-infobars')
-driver = webdriver.Chrome(executable_path="/test-Gmail/chromedriver.exe",
-                          options=chrome_options)
 
 
 def slow_typing(element, text):
@@ -22,31 +13,41 @@ def slow_typing(element, text):
         time.sleep(random.uniform(0, 0.3))
 
 
-try:
-    driver.get(url=url)
-    wait = WebDriverWait(driver, 20)
-    auth(driver, email, password)
+if __name__ == '__main__':
+    url = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
+    email = "viksharifullina@yandex.ru"
+    password = "BodnarchuK2801"
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = False
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('disable-infobars')
+    driver = webdriver.Chrome(executable_path="/test-Gmail/chromedriver.exe",
+                              options=chrome_options)
 
-    topic_email = "Приветствие"
-    search_input = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                                '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/div/span/input')))
+    try:
+        driver.get(url=url)
+        wait = WebDriverWait(driver, 20)
+        auth(driver, email, password)
 
-    slow_typing(search_input, topic_email)
-    buttonSearch = driver.find_element("xpath",
-                                       '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/button')
-    buttonSearch.click()
-    answer = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                          '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[1]/div')))
+        topic_email = "Приветствие"
+        search_input = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                                    '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/div/span/input')))
 
-    time.sleep(5)
-    if answer:
-        print("SearchLetters.py test is OK!")
-    else:
-        print("SearchLetters.py test is FAILED!")
+        slow_typing(search_input, topic_email)
+        buttonSearch = driver.find_element("xpath",
+                                           '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/button')
+        buttonSearch.click()
+        answer = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                              '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[1]/div')))
 
-except Exception as ex:
-    print(ex)
+        if answer:
+            print("SearchLetters.py test is OK!")
+        else:
+            print("SearchLetters.py test is FAILED!")
 
-finally:
-    driver.close()
-    driver.quit()
+    except Exception as ex:
+        print(ex)
+
+    finally:
+        driver.close()
+        driver.quit()
