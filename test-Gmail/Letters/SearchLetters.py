0fd9ebe669457a215471566ba992a33ff7587ead
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-12-08 13:34:49 +0300

    Add changed files for search messages

--- test-Gmail/Letters/SearchLetters.py
+++ test-Gmail/Letters/SearchLetters.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
-from SearchForNonExistentLetters import auth
+
 url = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
 email = "viksharifullina@yandex.ru"
 password = "BodnarchuK2801"
@@ -25,7 +25,22 @@ def slow_typing(element, text):
 try:
     driver.get(url=url)
     wait = WebDriverWait(driver, 20)
-    auth(driver, email, password)
+    button_email = driver.find_element("xpath",
+                                       '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[1]/div[1]/button')
+    button_email.click()
+    email_input = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                               '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[2]/div/div[2]/span/input')))
+
+    button_next = driver.find_element("xpath",
+                                      '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[4]/button')
+    slow_typing(email_input, email)
+    button_next.click()
+    password_input = wait.until(ec.visibility_of_element_located((By.XPATH,
+                                                                  '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[2]/div[1]/span/input')))
+    slow_typing(password_input, password)
+    button_login = driver.find_element("xpath",
+                                       '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button')
+    button_login.click()
 
     topic_email = "Приветствие"
     search_input = wait.until(ec.visibility_of_element_located((By.XPATH,
