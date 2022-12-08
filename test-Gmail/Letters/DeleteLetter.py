2a3a1d95e8508b1825490a4781228d06d3db934e
ViktoriaSharifullina <viktoriasharifullina@gmail.com>
2022-12-08 15:54:26 +0300

    Aded DeleteLetter.py

--- test-Gmail/Letters/DeleteLetter.py
+++ test-Gmail/Letters/DeleteLetter.py
@@ -1,9 +1,13 @@
 import time
 import random
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
 email = "viksharifullina@yandex.ru"
@@ -41,30 +45,11 @@ try:
     button_login = driver.find_element("xpath",
                                        '/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button')
     button_login.click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                "/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[1]/div/div/div/a/div/span[1]/div/div/div[2]"))).click()
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+                                                                '//*[@id="js-apps-container"]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[4]/div[2]/div/div[2]/div/div/div[5]'))).click()
 
-    topic_email = "Приветствие"
-    search_input = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                                '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/div/span/input')))
-
-    slow_typing(search_input, topic_email)
-    buttonSearch = driver.find_element("xpath",
-                                       '/html/body/div[3]/div[2]/div[7]/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[1]/form/button')
-    buttonSearch.click()
-    answer = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                          '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[1]/div')))
-
-    checkbox_enter = wait.until(ec.visibility_of_element_located((By.XPATH,'/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[3]/div/div[2]/div/div/div/a/div/span[1]/div/div/div[2]/label/input')))
-    checkbox_enter.click()
-    delete_button = wait.until(ec.visibility_of_element_located((By.XPATH,'/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[4]/div[2]/div/div[2]/div/div/div[5]')))
-    delete_button.click()
-    answer = wait.until(ec.visibility_of_element_located((By.XPATH,
-                                                          '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/div[2]/div/main/div[7]/div[1]/div/div/div[4]/div/div/div/h1/span')))
-
-    time.sleep(5)
-    if answer:
-        print("SearchLetters.py test is OK!")
-    else:
-        print("SearchLetters.py test is FAILED!")
 
 except Exception as ex:
     print(ex)
