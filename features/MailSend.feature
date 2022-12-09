761516a8afe334718577bf9b689907d4ef77939e
ViktoriaSharifullina <viktoriasharifullina@gmail.com>
2022-12-09 17:08:03 +0300

    Fix SendToSelf behave test

--- features/MailSend.feature
+++ features/MailSend.feature
@@ -2,13 +2,13 @@ Feature: MailSend
   # An authorized user should be able to send and receive messages
 
   Scenario Outline: SendToSelf
-    Given I'm authenticated user on inbox page
+    Given I am authenticated user on inbox page
     When I click on Write button
     And I fill Email field with user's email
     And I fill Subject field with <subject>
     And I fill Body field with <body>
     And Click Send button
-    And I should see a message that the letter has been sent
+    Then I should see a message that the letter has been sent
 
     Examples:
       |subject       |body                 |
--- steps/MailSend.py
+++ steps/MailSend.py
@@ -2,16 +2,13 @@ from behave import given, when, then, step
 from selenium import webdriver
 from functions import *
 
-
-
-
 URL = "https://passport.yandex.ru/auth/welcome?retpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821&backpath=https%3A%2F%2Fmail.yandex.ru%2F%3Fuid%3D1002378821%26noretpath%3D1&from=mail&origin=hostroot_homer_auth_ru"
 
-
-EMAIL ="mietpythontest@yandex.ru"
+EMAIL = "mietpythontest@yandex.ru"
 PASSWORD = "2323test"
 
-@given("I'm authenticated user on inbox page")
+
+@given("I am authenticated user on inbox page")
 def step_impl(context):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.headless = False
@@ -38,19 +35,21 @@ def step_impl(context):
 @when("I click on Write button")
 def step_impl(context):
     wait = WebDriverWait(context.driver, 40)
-    find_by_xpath(wait,context.driver,'/html/body/div[3]/div[2]/div[7]/div/div[3]/div/nav/div[2]/div/div/div/a').click()
+    find_by_xpath(wait,
+                  '/html/body/div[3]/div[2]/div[7]/div/div[3]/div/nav/div[2]/div/div/div/a').click()
 
 
 @step("I fill Email field with user's email")
 def step_impl(context):
     wait = WebDriverWait(context.driver, 40)
-    input_whom = find_by_xpath(wait,context.driver,"/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div/div[1]/div/div/div/div/div")
+    input_whom = find_by_xpath(wait,
+                               "/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div/div[1]/div/div/div/div/div")
     slow_typing(input_whom, EMAIL)
     input_whom.send_keys(Keys.RETURN)
 
 
 @step("I fill Subject field with {subject}")
-def step_impl(context,subject):
+def step_impl(context, subject):
     wait = WebDriverWait(context.driver, 40)
     inputTheme = wait.until(ec.visibility_of_element_located((By.XPATH,
                                                               '/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div[1]/div[1]/div[1]/div/div[3]/div/div/input')))
@@ -59,9 +58,10 @@ def step_impl(context,subject):
 
 
 @step("I fill Body field with {body}")
-def step_impl(context,body):
+def step_impl(context, body):
     wait = WebDriverWait(context.driver, 40)
-    inputMsg = find_by_xpath(wait,context.driver,'/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[2]/div[1]/div/div/div/div')
+    inputMsg = find_by_xpath(wait,
+                             '/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[2]/div[2]/div[1]/div/div/div/div')
     slow_typing(inputMsg, body)
     inputMsg.send_keys(Keys.RETURN)
 
@@ -69,13 +69,14 @@ def step_impl(context,body):
 @step("Click Send button")
 def step_impl(context):
     wait = WebDriverWait(context.driver, 40)
-    find_by_xpath(wait,context.driver,'/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/button').click()
+    find_by_xpath(wait,
+                  '/html/body/div[3]/div[2]/div[10]/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/div/div[1]/div[1]/button').click()
 
 
 @then("I should see a message that the letter has been sent")
 def step_impl(context):
     wait = WebDriverWait(context.driver, 40)
-    done = find_by_xpath(wait,context.driver,'/html/body/div[16]/div/div[2]/div[1]/span')
+    done = find_by_xpath(wait, '/html/body/div[16]/div/div[2]/div[1]/span')
     if done:
         print("SendingLetterForOwnMailbox test is OK!")
     else:
